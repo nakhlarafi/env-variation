e3150f890422e0ce42fe98f9bdd846017832c19f
nakhlarafi <nakhla054@gmail.com>
2023-11-08 19:09:57 -0500

    added script to run in the cluster

---
+++ job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -J Math
+#SBATCH --mem=100M 
+#SBATCH -w virya3
+#SBATCH -o _%x%J.out
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=nakhla054@gmail.com
+source /etc/profile.d/modules.sh # adding module binaries
+module load anaconda/3.2023.03
+
+python3 runtotal.py Math
+
+nvidia-smi
